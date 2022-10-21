8b4cd45ac37ee5ac33d6cb248390373fe6636bd3
shas232 <79205176+shas232@users.noreply.github.com>
2022-10-21 19:39:01 +0530

    upload

---
+++ src/components/Projects.js
@@ -0,0 +1,21 @@
+
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import colorSharp3 from "../assets/img/color-sharp3.svg";
+import 'animate.css';
+
+
+export const Projects = () => {
+
+  
+
+  return (
+    <section className="project" id="project">
+     
+      <img className="background-image-right" src={colorSharp2}></img>
+      
+      <div className="try">
+      <img className="background-image-right" src={colorSharp3}></img>
+      </div>
+    </section>
+  )
+}
---
+++ src/components/Skills.js
@@ -0,0 +1,15 @@
+
+import 'react-multi-carousel/lib/styles.css';
+
+import colorSharp from "../assets/img/color-sharp.png"
+
+export const Skills = () => {
+  
+
+  return (
+    <section className="skill" id="skills">
+        
+        <img className="background-image-left" src={colorSharp} alt="Image" />
+    </section>
+  )
+}
